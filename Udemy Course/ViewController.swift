58254d114b37ecac5aa147a1374b9d80516bf2c6
YoderDaniel <danielbyoder@gmail.com>
2016-09-19 21:06:55 +0900

    Now we can add numbers

--- Udemy Course/ViewController.swift
+++ Udemy Course/ViewController.swift
@@ -6,21 +6,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
-        
-        tapCount = tapCount + 1
-        print(tapCount)
-        if tapCount >= 10 {
-            theLabel.text = "Stop Pushing Me!"
-        }
-        
+        print(text1.text!)
+        print(text2.text!)
+        theLabel.text = "The Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+     
     }
+
+
     
-    @IBAction func secondButton(_ sender: AnyObject) {
-         theLabel.text = "I've been changed twice!"
-    }
+ 
     
     
     override func viewDidLoad() {
